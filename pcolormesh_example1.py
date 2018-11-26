8c88ea05598160d199d65e36a824f77f5ca263e0
marta.bertran <martabertrangarcia@gmail.com>
2018-11-26 09:14:51 +0100

    Create pcolormesh_example1.py

---
+++ pcolormesh_example1.py
@@ -0,0 +1,9 @@
+#Example to plot a map from a NetCDF4 using matplotlib.pyplot.pcolormesh
+import glob
+import netCDF4
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+
+%f0 indicates the location of the .nc file
+f0 = '\users\marta...'
+
