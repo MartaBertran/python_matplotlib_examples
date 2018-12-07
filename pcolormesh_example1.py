6de4719df10e0da571802862040bc5f8cb44354f
marta.bertran <martabertrangarcia@gmail.com>
2018-12-07 14:04:49 +0100

    Update pcolormesh_example1.py

--- pcolormesh_example1.py
+++ pcolormesh_example1.py
@@ -1,9 +1,17 @@
-#Example to plot a map from a NetCDF4 using matplotlib.pyplot.pcolormesh
-import glob
-import netCDF4
-from netCDF4 import Dataset
+#Example from examples in matplotlib.org 
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
+import Numpy as np
 
-%f0 indicates the location of the .nc file
-f0 = '\users\marta...'
+#make these smaller to increase resolution
+
+#generate 2 2D grids for the x and y bounds
+
+#x and y are bounds, so z shoul be the value inside thouse bounds
+
+#pick the desired colormap, sensible levels and define a normalization
+
+
 
