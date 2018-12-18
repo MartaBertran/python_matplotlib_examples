103abb89477609a2c762631f5ca1b5faa070b615
marta.bertran <martabertrangarcia@gmail.com>
2018-12-18 16:10:54 +0100

    Create 9_16_scatter_plot.py

---
+++ 9_16_scatter_plot.py
@@ -0,0 +1,18 @@
+# 9.16. SCATTER PLOT
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# Data
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+# O to 15 point radii
+area = (30*np.random.rand(N))**2
+
+# Scatter Plot
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+plt.show()
